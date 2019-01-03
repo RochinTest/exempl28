28916ed572edba2ffc606a4b3f0455c4b776b767
Rimlin <Rimlin@gmail.com>
2019-01-03 10:25:16 +0200

    task 37

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,56 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
-//Создайте массив из 11 случайных целых чисел из отрезка [-1;1],
-// выведите массив на экран в строку. Определите какой элемент
-// встречается в массиве чаще всего и выведите об этом сообщение на экран.
-// Если два каких-то элемента встречаются одинаковое количество раз, то не выводите ничего.
+//). Пользователь должен указать с клавиатуры чётное положительное число,
+// а программа должна создать массив указанного размера из случайных целых
+// чисел из [-5;5] и вывести его на экран в строку. После этого программа
+// должна определить и сообщить пользователю о том, сумма модулей какой
+// половины массива больше: левой или правой, либо сообщить, что эти суммы
+// модулей равны. Если пользователь введёт неподходящее число, то программа
+// должна требовать повторного ввода до тех пор, пока не будет указано корректное значение.
 public class Main {
 
     public static void main(String[] args) {
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        int[] s = new int[11];
+        int m = 0, n, l;
 
-        for (int i = 0; i < 11; i++) {
-            s[i] = (int) (Math.random() * (-4) + 2);
-            System.out.print(" " + s[i]);
-            if (s[i] == -1) {
-                a++;
-            } else if (s[i] == 0) {
-                b++;
-            } else if (s[i] == 1) {
-                c++;
+        System.out.println("Введите чётное положительное число:");
+        Scanner sc = new Scanner(System.in);
+        M:
+        for (int i = 0; i < 10; i++) {
+            if (sc.hasNextInt()) {
+                m = sc.nextInt();
             }
+
+            if (m > 0 && m % 2 == 0) {
+                break M;
+            } else System.out.println("Число не верное,Введите чётное положительное число ");
         }
-        System.out.println();
-        if (a != b && a != c && b != c) {
-            if (a > b & a > c) {
-                System.out.println("Чаще встречается -1 ");
-            } else if (b > a & b > c) {
-                System.out.println("Чаще встречается 0 ");
-            } else if (c > a & c > b) {
-                System.out.println("Чаще встречается 1 ");
+        int[] s = new int[m];
+        n = m / 2;
+        int sumleft = 0;
+        int sumraid = 0;
+        for (int i = 0; i < s.length; i++) {
+            s[i] = (int) (Math.random() * 12 - 6);
+            System.out.print(" " + s[i]);
+            if (n <= i) {
+                sumleft = sumleft + s[i];
+            } else {
+                sumraid = sumraid + s[i];
             }
+        }System.out.println();
+        if (sumleft < sumraid) {
+            System.out.println("левая сумма больше");
+        } else if (sumleft > sumraid) {
+            System.out.println("правая сумма больше");
+        } else if (sumleft == sumraid) {
+            System.out.println(" суммы равны");
         }
     }
-
-
 }
+
+
+
+
+
 
 
 
