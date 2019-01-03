94d03f39b8cb6615f3e720057bc7308136dd42d3
Rimlin <Rimlin@gmail.com>
2019-01-03 20:26:19 +0200

    task 38

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,67 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
-//). Пользователь должен указать с клавиатуры чётное положительное число,
-// а программа должна создать массив указанного размера из случайных целых
-// чисел из [-5;5] и вывести его на экран в строку. После этого программа
-// должна определить и сообщить пользователю о том, сумма модулей какой
-// половины массива больше: левой или правой, либо сообщить, что эти суммы
-// модулей равны. Если пользователь введёт неподходящее число, то программа
-// должна требовать повторного ввода до тех пор, пока не будет указано корректное значение.
+// Программа должна создать массив из 12 случайных целых чисел из отрезка [-10;10]
+// таким образом, чтобы отрицательных и положительных элементов там было поровну
+// и не было нулей. При этом порядок следования элементов должен быть случаен
+// (т. е. не подходит вариант, когда в массиве постоянно выпадает сначала 6 положительных,
+// а потом 6 отрицательных чисел или же когда элементы постоянно чередуются через один и пр.).
+// Вывести полученный массив на экран.
 public class Main {
 
     public static void main(String[] args) {
-        int m = 0, n, l;
+        int m = 0, n = 0;
+        int j = 0;
 
-        System.out.println("Введите чётное положительное число:");
-        Scanner sc = new Scanner(System.in);
-        M:
-        for (int i = 0; i < 10; i++) {
-            if (sc.hasNextInt()) {
-                m = sc.nextInt();
+        // System.out.println("Введите чётное положительное число:");
+        // Scanner sc = new Scanner(System.in);
+        // M:
+        //for (int i = 0; i < 10; i++) {
+        //  if (sc.hasNextInt()) {
+        // m = sc.nextInt();
+        //  }
+
+        //if (m > 0 && m % 2 == 0) {
+        // break M;
+        // } else System.out.println("Число не верное,Введите чётное положительное число ");
+        // }
+        int[] s = new int[10];
+        int i = 0;
+
+        while (i < 10) {
+
+            s[i] = (int) (Math.random() * 22 - 11);
+            if (s[i] == 0) {
+                continue;
+            } else if (s[i] > 0) {
+                m++;
+            } else if (s[i] < 0) {
+                n++;
             }
 
-            if (m > 0 && m % 2 == 0) {
-                break M;
-            } else System.out.println("Число не верное,Введите чётное положительное число ");
-        }
-        int[] s = new int[m];
-        n = m / 2;
-        int sumleft = 0;
-        int sumraid = 0;
-        for (int i = 0; i < s.length; i++) {
-            s[i] = (int) (Math.random() * 12 - 6);
             System.out.print(" " + s[i]);
-            if (n <= i) {
-                sumleft = sumleft + s[i];
-            } else {
-                sumraid = sumraid + s[i];
-            }
-        }System.out.println();
-        if (sumleft < sumraid) {
-            System.out.println("левая сумма больше");
-        } else if (sumleft > sumraid) {
-            System.out.println("правая сумма больше");
-        } else if (sumleft == sumraid) {
-            System.out.println(" суммы равны");
+            i++;
         }
+        System.out.println();
+        System.out.println(m + " " + n);
+        m = 0;n=0;
+        while (j < 10) {
+            if (m > n && s[j] > 0) {
+                s[j] *= -1;
+            }else if (m<n && s[j]<0){s[j]*=-1;}
+            if (s[j] > 0) {
+                m++;
+            } else if (s[j] < 0) {
+                n++;
+            }
+            System.out.print(" " + s[j]);
+            j++;
+        }System.out.println();
+        System.out.println(m + " " + n);
     }
+
 }
+
+
 
 
 
